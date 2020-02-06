566ca370d0853f943ce5270fa6acd0ebcedbee0b
Dilan  Nana <nana.dilan@gmail.com>
2020-02-05 19:27:34 -0800

    Updates

---
+++ precache-manifest.ea6a7e6e4caec525cb96c0c403d72dfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032ff85c804e7dda936e5551f42a81e6",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "cbd25686054d50847c99",
+    "url": "/todo-app/static/css/main.116554ab.chunk.css"
+  },
+  {
+    "revision": "3155d5d9528c49cb7d3c",
+    "url": "/todo-app/static/js/2.d87bcce2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-app/static/js/2.d87bcce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd25686054d50847c99",
+    "url": "/todo-app/static/js/main.cf887130.chunk.js"
+  },
+  {
+    "revision": "7dbae3c775b13dc5e751",
+    "url": "/todo-app/static/js/runtime-main.2620d507.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
